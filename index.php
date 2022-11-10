6ee1a1949175a3649f1b6993d679cca2477b5990
faithakinyi <ogembofaith12@gmail.com>
2022-11-10 13:18:14 +0300

    php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+echo "Hello world program";
+
+$myName = "Faith Akinyi";
+echo $myName;
+var_dump($myName);
+
+// interger
+$a = 30;
+$b = "30";
+$c = true;
+$d = 200.34;
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+
+echo "My Name is ".$myName. ". My M-PESA balance is Ksh.".$d;
+?>
